a714b8c9dce0476ac8a241969efd7fce4e250d72
Ravi Fonseca <ravi.fonseca@wrightia.com>
2022-12-31 13:33:05 +0000

    pratica de exception handling

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,51 +1,28 @@
 package application;
 
-import model.entities.Reservation;
-import model.exceptions.DomainException;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        try {
-            System.out.print("Room number: ");
-            int roomNr = sc.nextInt();
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            Date checkin = sdf.parse(sc.next());
-            System.out.print("Check-out date (dd/MM/yyyy): ");
-            Date checkout = sdf.parse(sc.next());
+        //*** PRIMEIRO RODEO COM AS EXCEPTIONS ***//
 
-
-            Reservation res = new Reservation(roomNr, checkin, checkout);
-            System.out.println(res);
-
-            System.out.println("\nEnter data to update the reservation:");
-
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            checkin = sdf.parse(sc.next());
-            System.out.print("Check-out date (dd/MM/yyyy): ");
-            checkout = sdf.parse(sc.next());
-
-            Date now = new Date();
-
-            res.updateDates(checkin, checkout);
-            System.out.println(res);
+        /*
+        try{
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Position  - "+e);
+        } catch (InputMismatchException e){
+            System.out.println("Invalid Input - "+e);
         }
-        catch (ParseException e){
-            System.out.println("Invalid date format");
-        }
-        catch (DomainException e){
-            System.out.println("Error in reservation: "+e.getMessage());
-        }
-        catch (RuntimeException e ){
-            System.out.println("Unexpected error");
-        }
+
+        System.out.println("End of program");*/
+
 
 
 
--- src/model/exceptions/DomainException.java
+++
@@ -1,9 +0,0 @@
-package model.exceptions;
-
-public class DomainException extends RuntimeException{
-
-    public DomainException(String msg){
-        super(msg);
-    }
-
-}
