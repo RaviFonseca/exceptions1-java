afea208dc538c0af449a076a7393e5a628599343
Ravi Fonseca <ravi.fonseca8@gmail.com>
2023-01-03 00:37:16 +0000

    added final solution and practical exercise

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import accountPack.Account;
 import model.entites.Reservation;
 import model.exceptions.DomainException;
 
@@ -7,14 +8,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         //*** PRIMEIRO RODEO COM AS EXCEPTIONS ***//
-
         /*
         try{
             String[] vect = sc.nextLine().split(" ");
@@ -27,13 +29,8 @@ public class Program {
         }
 
         System.out.println("End of program");*/
-
-
         //** Solucão ruim do Reservation **//
-
-
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        /*SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         try {
             System.out.print("Room number: ");
@@ -62,7 +59,33 @@ public class Program {
             System.out.println(e.getMessage());
         } catch (RuntimeException e){
             System.out.println("Unexpected error");
+        }*/
+        //** Exercicio de fixação para o chapter de Exceptions **//
+
+        try{
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder: ");
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            double withdrawLimit = sc.nextDouble();
+
+            Account acc = new Account(number,holder,balance,withdrawLimit);
+
+            System.out.print("\nEnter amount for withdraw: ");
+            double amount = sc.nextDouble();
+            acc.withdraw(amount);
+            System.out.println("New balance: "+String.format("%.2f",acc.getBalance()));
+        } catch (DomainException e){
+            System.out.println(e.getMessage());
+        }catch (RuntimeException e){
+            System.out.println("Unexpected error: "+e);
         }
+
 
 
         ///
